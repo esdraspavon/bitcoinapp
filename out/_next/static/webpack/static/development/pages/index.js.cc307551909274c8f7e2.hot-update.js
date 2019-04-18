d002a44fcb258113d6264294ca8d39a7a45faadb
Esdras Pavon <pavonesdras@gmail.com>
2019-04-18 18:28:29 -0300

    gh-pages deploy

---
+++ out/_next/static/webpack/static/development/pages/index.js.cc307551909274c8f7e2.hot-update.js
@@ -0,0 +1,39 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Event.js":
+/*!*****************************!*\
+  !*** ./components/Event.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/esdras/Documentos/bitcoinapp/components/Event.js";
+
+
+var Event = function Event(props) {
+  var _props$data = props.data,
+      name = _props$data.name,
+      url = _props$data.url,
+      description = _props$data.description;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: url,
+    target: "_blank",
+    className: "list-group-item active text-light mb-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Event);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.cc307551909274c8f7e2.hot-update.js.map
